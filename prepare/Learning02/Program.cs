96e61c45b7e33fbb489e92ebbb6660e405fff969
Jegs01 <bodcafe@gmail.com>
2024-05-02 10:31:24 +0100

    Added my resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Airman";
+        job1._company = "Nigerian Air Force";
+        job1._startYear = 2012;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "IT Assistant";
+        job2._company = "Mass Medical Mission";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Kolawole Jegede";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
